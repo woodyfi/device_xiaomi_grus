596eca98c1816dfca8b1faac566bd6acb34bce4f
Georg Veichtlbauer <georg@vware.at>
2024-01-30 09:29:36 +0100

    sdm710-common: powerhint: Add DT2W
    Change-Id: I7d0dfe249798864a39afd4f339b692584faaf3ec

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -101,9 +101,6 @@ BOARD_VENDOR := xiaomi
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := sdm710
 
-# Power
-TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
-
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
