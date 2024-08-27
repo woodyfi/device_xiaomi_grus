ad492458c45df84eeb8140ae1b6419df1e7d1c87
ELbee <85691313+woodyfi@users.noreply.github.com>
2024-08-27 14:57:24 +0700

    Update and rename afterlife_grus.mk to orion_grus.mk

--- orion_grus.mk
+++ orion_grus.mk
@@ -9,17 +9,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from grus device
 $(call inherit-product, device/xiaomi/grus/device.mk)
-
-AFTERLIFE_BUILD_TYPE := OFFICIAL
-AFTERLIFE_MAINTAINER := Sweetbtrfly
+ORION_MAINTAINER := PH8+
+ORION_MAINTAINER_LINK := t.me/blast_dao
+ORION_BUILD_TYPE := UNOFFICIAL
+ORION_GAPPS := false
+TARGET_ENABLE_BLUR := true
 TARGET_BOOT_ANIMATION_RES := 1080
-USE_PIXEL_CHARGING := true
+TARGET_HAS_UDFPS :=  true
 
-PRODUCT_NAME := afterlife_grus
+BUILD_GOOGLE_CONTACTS := true
+BUILD_GOOGLE_DIALER := true
+BUILD_GOOGLE_MESSAGE := true
+
+PRODUCT_NAME := orion_grus
 PRODUCT_DEVICE := grus
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
@@ -28,16 +34,7 @@ PRODUCT_MODEL := Mi 9 SE
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Device config
-TARGET_HAS_UDFPS := true
-TARGET_ENABLE_BLUR := true
-TARGET_EXCLUDES_AUDIOFX := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-
-# TARGET_BUILD_PACKAGE options:
-# 1 - vanilla (default)
-# 2 - microg
-# 3 - gapps
-TARGET_BUILD_PACKAGE := 1
 
 # TARGET_LAUNCHER options:
 # 1 - stock (default)
@@ -54,8 +51,6 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 # Debugging
 TARGET_INCLUDE_MATLOG := false
-
-ALPHA_MAINTAINER := elpaablo
 
 PRODUCT_ENFORCE_VINTF_MANIFEST := false
 PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
